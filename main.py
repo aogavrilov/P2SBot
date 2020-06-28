ee4223171ba2b7bbb8258dfbf10bd2ac6cfc44eb
alexgavrilov28@gmail.com <alexgavrilov28@gmail.com>
2020-06-29 01:10:13 +0300

    Added style transfer

--- main.py
+++ main.py
@@ -4,25 +4,32 @@ import json
 from aiogram import Bot, Dispatcher, executor, types
 from PIL import Image
 import os
-
+import sklearn
+import sklearn.preprocessing
 from aiogram.types import ChatActions
+
+from styletr import StyleTransfer
 
 tgTok = None
 deepmxTok = None
 import numpy as np
-import sklearn
-import sklearn.preprocessing
+
+
 import deepmux
-#import torch
-#from torchvision import transforms
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+from torchvision import transforms
+#logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 with open("config.json", "r") as readcfg:
     data = json.load(readcfg)
     tgTok = data['tgTok']
     deepmxTok = data['deepmxTok']
-tgTok = '1139411645:AAFLTOA3AoWkoLjdUvzXtRZpDGx3mOJPTe8'
+tgTok = '1159135693:AAFf_A3pLRsBfdkVVzRYRMbVeVEvzE-RFjQ'
 bot = Bot(token=tgTok)
 disp = Dispatcher(bot)
+
+
+styles = set()
+cycle = set()
+styles_ = set()
 
 
 class SomeModel:
@@ -53,12 +60,15 @@ class SomeModel:
         input_batch = input_tensor.unsqueeze(0)
         return input_batch.numpy()
 
+model = SomeModel("P2S")
+
 
 @disp.message_handler(commands="start")
 async def start_dialog(message: types.Message):
     poll_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     poll_keyboard.add(types.KeyboardButton("О приложении"))
-    poll_keyboard.add(types.KeyboardButton("Преобразовать фото в скетч"))
+    poll_keyboard.add(types.KeyboardButton("Style Transfer"))
+    poll_keyboard.add(types.KeyboardButton("Преобразовать фото в скетч(криво увы)"))
     # poll_keyboard.add(types.KeyboardButton("Поддержать"))
     poll_keyboard.add(types.KeyboardButton("Отмена"))
     await message.answer("Выберите действие, которое хотите совершить", reply_markup=poll_keyboard)
@@ -79,14 +89,32 @@ async def echo(message: types.Message):
                              "Построение изображения может занять пару секунд. Когда приложение завершит свою работу, "
                              "оно пришлет вам результат. \nДанное приложение создано в рамках выпускного проекта DLS.\n"
                              "Пример работы программы вы можете видеть ниже:")
-    elif (message.text == "Преобразовать фото в скетч"):
+    elif (message.text == "Преобразовать фото в скетч(криво увы)"):
+        if(message.from_user.id in styles_):
+            styles_.remove(message.from_user.id)
+        if(message.from_user.id in styles):
+            styles.remove(message.from_user.id)
         await message.answer("Отправьте ОДНО фото, которое хотите преобразовать в скетч")
+        cycle.add(message.from_user.id)
+
+    elif(message.text == "Style Transfer"):
+        if(message.from_user.id in cycle):
+            cycle.remove(message.from_user.id)
+        await message.answer("Отправьте ОДНО фото, на которое хотите наложить стиль")
+        styles.add(message.from_user.id)
 
     elif (message.text == "Отмена"):
         tmp = types.ReplyKeyboardRemove()
         await message.answer(
             "Спасибо, что воспользовались нашим приложением. Вы всегда можете ввести /start и продолжить развлекаться.",
             reply_markup=tmp)
+        if (message.from_user.id in styles_):
+            styles_.remove(message.from_user.id)
+        if (message.from_user.id in styles):
+            styles.remove(message.from_user.id)
+        if (message.from_user.id in cycle):
+            cycle.remove(message.from_user.id)
+
     else:
         await message.answer("Данная команда неизвестна. Введите /start для отображения меню.")
         print(message.text)
@@ -94,21 +122,57 @@ async def echo(message: types.Message):
 
 @disp.message_handler(content_types=["photo"])
 async def photo_ed(message: types.Message):
-    model = SomeModel("P2S")
-    await message.photo[-1].download("images/" + str(message.from_user.id) + '.jpg')
-    img = open("images/" + str(message.from_user.id) + '.jpg', 'rb')
-    h, w = img.size
-    # print(img.shape)
-    img = model(img)
-    img = np.transpose(img, (1, 2, 0))
-    img = Image.fromarray((img * 255).astype(np.uint8))
-    t = transforms.Compose([
-        transforms.Resize((h, w)),
-        transforms.ToTensor()
-    ])
-    img = t(img)
-    img = np.transpose(img, (1, 2, 0))
-    await bot.send_photo(message.from_user.id, img, caption="Преобразованное фото")
+    if(message.from_user.id in cycle):
+
+        await message.photo[-1].download("images/" + str(message.from_user.id) + '.jpg')
+        img = Image.open('images/'+ str(message.from_user.id) + '.jpg')
+        #open("images/" + str(message.from_user.id) + '.jpg', 'rb')
+        h, w = img.size
+        # print(img.shape)
+        t1 = transforms.Compose([
+            transforms.Resize((512, 512)),
+          #  transforms.ToTensor()
+        ])
+        img = t1(img)
+        img = model(img)
+        img = np.transpose(img, (1, 2, 0))
+        img = Image.fromarray((img * 255).astype(np.uint8))
+        t = transforms.Compose([
+            transforms.Resize((w, h)),
+            transforms.ToTensor()
+        ])
+        img = t(img).numpy()
+        img = np.transpose(img, (1, 2, 0))
+        img = Image.fromarray(np.uint8(img * 255))
+        img.save('images/'+ str(message.from_user.id) + '.jpg')
+        img_ = open('images/'+ str(message.from_user.id) + '.jpg', 'rb')
+        await bot.send_photo(message.from_user.id, img_, caption="Преобразованное фото")
+        cycle.remove(message.from_user.id)
+        os.remove("images/" + str(message.from_user.id) + '.jpg')
+    elif(message.from_user.id in styles):
+        await message.photo[-1].download("images/" + str(message.from_user.id) + '.jpg')
+        styles.remove(message.from_user.id)
+        styles_.add(message.from_user.id)
+        await message.answer("Принято! Отправь еще фотку стиля, который нужно перенести.")
+    elif(message.from_user.id in styles_):
+        await message.photo[-1].download("images/" + str(message.from_user.id) + 'style.jpg')
+        #img = Image.open('images/' + str(message.from_user.id) + '.jpg')
+        await message.answer("Отлично! Осталось подождать 5-10 минут и нейросеть пришлет результат.")
+        nm = StyleTransfer("images/" + str(message.from_user.id) + '.jpg', "images/" + str(message.from_user.id) + 'style.jpg')
+        x = nm.getRes()
+
+        x = Image.fromarray((x.detach().numpy().squeeze(0).transpose((1, 2, 0)) * 255).astype(np.uint8))
+        x.save("images/" + str(message.from_user.id) + '.jpg')
+        img_ = open('images/' + str(message.from_user.id) + '.jpg', 'rb')
+        await bot.send_photo(message.from_user.id, img_, caption="Преобразованное фото")
+        await styles_.remove(message.from_user.id)
+        os.remove("images/" + str(message.from_user.id) + '.jpg')
+        os.remove( "images/" + str(message.from_user.id) + 'style.jpg')
+    else:
+        await message.answer("Ой, ты не выбрал, что хочешь сделать с картинкой. Введи /start и выбери на клавиатуре действие!")
+
+
+
 
 
 if __name__ == "__main__":
---
+++ styletr.py
@@ -0,0 +1,175 @@
+import lib.copy as copy
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.models as models
+from torchvision import transforms
+from PIL import Image
+
+
+def gram_matrix(input):
+    batch_size, h, w, f_map_num = input.size()  # batch_size = 1
+    # h, w - dimensions of a feature map
+    # f_map_num - number of a future map
+    features = input.view(batch_size * h, w * f_map_num)
+    G = torch.mm(features, features.t())  # matrix multiplicate for gram matrix
+    G = G.div(batch_size * h * w * f_map_num)  # normalize Gramm matrix
+    return G
+
+
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        return (img - self.mean) / self.std  # normalize img
+
+class ContentLoss(nn.Module):
+    def __init__(self, target, ):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()  # detach from calculation graph
+        self.loss = F.mse_loss(self.target, self.target)  # some initialization
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)  # mse from target image
+        # and content at the moment image
+        return input
+
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()  # detach from calculation graph
+        self.loss = F.mse_loss(self.target, self.target)  # some initialization
+
+    def forward(self, input):
+        G = gram_matrix(input)  # G-matrix features
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+class StyleTransfer:
+
+    def image_loader(self,image_name):
+        imsize = 256
+        loader = transforms.Compose([
+            transforms.Resize(imsize),
+            transforms.CenterCrop(imsize),
+            transforms.ToTensor()
+        ])
+        image = Image.open(image_name)
+        image = loader(image).unsqueeze(0)  # Добавляем размерность(размер батча=1) для тензора
+        return image
+
+
+    cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406])
+    cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225])
+
+
+
+
+    content_layers_default = ['conv_4']
+    style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+    cnn = models.vgg19(pretrained=True).features.eval()
+
+    def get_input_optimizer(self, input_img):
+        optimizer = optim.LBFGS([input_img.requires_grad_()])
+        return optimizer
+
+    def get_style_model_and_losses(self, cnn, normalization_mean, normalization_std,
+                                   style_img, content_img,
+                                   content_layers=content_layers_default,
+                                   style_layers=style_layers_default):
+        cnn = copy.deepcopy(cnn)
+        normalization = Normalization(normalization_mean, normalization_std)
+        content_losses = []
+        style_losses = []
+        model = nn.Sequential(normalization)
+
+        i = 0
+        for layer in cnn.children():
+            if isinstance(layer, nn.Conv2d):
+                i += 1
+                name = 'conv_{}'.format(i)
+            elif isinstance(layer, nn.ReLU):
+                name = 'relu_{}'.format(i)
+                layer = nn.ReLU(inplace=False)
+            elif isinstance(layer, nn.MaxPool2d):
+                name = 'pool_{}'.format(i)
+            elif isinstance(layer, nn.BatchNorm2d):
+                name = 'bn_{}'.format(i)
+            else:
+                raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+            model.add_module(name, layer)
+            if name in content_layers:
+                target = model(content_img).detach()
+                content_loss = ContentLoss(target)
+                model.add_module("content_lossL{}".format(i), content_loss)
+                content_losses.append(content_loss)
+
+            if name in style_layers:
+                target_feature = model(style_img).detach()
+                style_loss = StyleLoss(target_feature)
+                model.add_module("style_loss_{}".format(i), style_loss)
+                style_losses.append(style_loss)
+        for i in range(len(model) - 1, -1, -1):
+            if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+                break
+        model = model[:(i + 1)]
+        return model, style_losses, content_losses
+
+
+    def run_style_transfer(self, cnn, normalization_mean, normalization_std,
+                           content_img, style_img, input_img, num_steps=50,
+                           style_weight=100000, content_weight=1):
+        model, style_losses, content_losses = self.get_style_model_and_losses(cnn, normalization_mean, normalization_std,
+                                                                         style_img, content_img)
+        optimizer = self.get_input_optimizer(input_img)
+        run = [0]
+        while run[0] <= num_steps:
+            def closure():
+                input_img.data.clamp_(0, 1)
+                optimizer.zero_grad()
+                model(input_img)
+
+                style_score = 0
+                content_score = 0
+
+                for sl in style_losses:
+                    style_score += sl.loss
+                for cl in content_losses:
+                    content_score += cl.loss
+
+                style_score *= style_weight
+                content_score *= content_weight
+
+                loss = style_score + content_score
+                loss.backward()
+                run[0] += 1
+                if run[0] % 50 == 0:
+                    print('run {}:'.format(run))
+                    print('Style Loss: {:4f} Content Loss: {:4f}'.format(style_score.item(),
+                                                                         content_score.item()))
+                    print()
+                return style_score + content_score
+
+            optimizer.step(closure)
+        input_img.data.clamp_(0, 1)
+        return input_img
+
+    def __init__(self, pic, style):
+        self.style_img = self.image_loader(style)
+        self.content_img = self.image_loader(pic)
+        self.input_img = self.content_img.clone()
+
+
+    def getRes(self):
+        self.output = self.run_style_transfer(self.cnn, self.cnn_normalization_mean,
+                                              self.cnn_normalization_std, self.content_img, self.style_img,
+                                              self.input_img)
+        return self.output
+
+#(img * 255).astype(np.uint8)
